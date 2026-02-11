adb5b16faebe21490b4d8f0503b13ee7c45173c1
akitohidata 211228 <yarey614@gmail.com>
2026-02-11 13:19:08 +0700

    update project

--- router/random/china.ts
+++ router/random/china.ts
@@ -1,39 +1,21 @@
-import axios from "axios"
-import { sendBinary } from "./sendBinary"
+import { Request, Response } from 'express';
 
-async function getRandomCecanChinaImage() {
-  const URL =
-    "https://raw.githubusercontent.com/siputzx/Databasee/refs/heads/main/cecan/china.json"
+export default async function waifuHandler(req: Request, res: Response) {
+    try {
+        const apiResponse = await fetch("https://raw.githubusercontent.com/siputzx/Databasee/refs/heads/main/cecan/china.json");
+        if (!apiResponse.ok) throw new Error("Gagal mengambil data");
+        
+        const json: any = await apiResponse.json();
+        
+        const imageResponse = await fetch(json.url);
+        const arrayBuffer = await imageResponse.arrayBuffer();
+        const buffer = Buffer.from(arrayBuffer);
+        
+        res.set('Content-Type', 'image/png');
+        res.send(buffer);
 
-  const { data } = await axios.get(URL, { timeout: 30000 })
-
-  if (!Array.isArray(data) || !data.length) {
-    throw new Error("Image list empty")
-  }
-
-  const randomUrl = data[Math.floor(Math.random() * data.length)]
-
-  const img = await axios.get(randomUrl, {
-    responseType: "arraybuffer",
-    timeout: 30000,
-  })
-
-  return Buffer.from(img.data)
-}
-
-export default [
-  {
-    metode: "GET",
-    endpoint: "/api/r/cecan/china",
-    name: "cecan china",
-    async run({ res }) {
-      try {
-        const buffer = await getRandomCecanChinaImage()
-        sendBinary(res, buffer, "image/jpeg", "cecan-china.jpg")
-      } catch (e: any) {
-        res.statusCode = 500
-        res.end(JSON.stringify({ status: false, message: e.message }))
-      }
-    },
-  },
-]
+    } catch (error: any) {
+        console.error("Error waifu:", error);
+        res.status(500).json({ status: false, message: "Internal Server Error" });
+    }
+};
